6f274f9ba75681fe4454b9e3d68c5e364136ed6e
Leonard <leonard.flachs@gmail.com>
2020-03-01 21:32:48 +0100

    Structure of the project

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../Components/Cards/Card";
+
+function Homepage() {
+  return (
+    <Card
+      cardTitle={"Lassi Khara"}
+      cardSubtitle={"Main ingredient"}
+      cardImage={
+        "https://www.thecocktaildb.com/images/media/drink/m1suzm1487603970.jpg"
+      }
+    />
+  );
+}
+
+export default Homepage;
